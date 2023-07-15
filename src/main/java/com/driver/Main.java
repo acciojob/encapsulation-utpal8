458b3bf4e9f5c1c1940ec001f7818f7c331af13d
Utpal <08utpal@gmail.com>
2023-07-15 13:48:13 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        // Task 3: create an object of class RWOnly
+        RWOnly obj = new RWOnly();
+
+        // Task 4: try setting a value to name by directly accessing it using obj
+        // obj.name = "John"; // Error: The variable name is private and cannot be accessed directly
+
+        // Task 5: try implementing getter and setter member functions in class RWOnly
+        obj.setName("John");
+        System.out.println("Name: " + obj.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Task 5: implement getter and setter functions
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
